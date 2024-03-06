783c6a9549baa466e269d46f5a8a9e0e4434bdbd
arthur <arthur.rios007@gmail.com>
2024-03-06 15:08:20 -0300

    chore: add default opengraph image

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,7 +5,7 @@ import Stripe from 'stripe'
 import { Button } from '@/components/button'
 import { getProduct } from '@/services/getProduct'
 
-interface ProductPageProps {
+export interface ProductPageProps {
   params: {
     id: string
   }
@@ -32,7 +32,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
 
   return (
     <main className="mx-auto grid max-w-main grid-cols-2 items-stretch gap-16">
-      <div className="h-container flex w-full max-w-[576px] items-center justify-center rounded-lg bg-gradient-to-b from-[#1EA483] to-[#7465D4] p-1">
+      <div className="flex h-container w-full max-w-[576px] items-center justify-center rounded-lg bg-gradient-to-b from-[#1EA483] to-[#7465D4] p-1">
         <Image src={product.images[0]} width={520} height={480} alt="" />
       </div>
       <div className="flex flex-col">
